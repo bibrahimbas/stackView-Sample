95eb595568a4ff0592991de750284884ec505d34
mac <belmaibrahimbas@Belma-MacBook-Pro.local>
2017-08-05 23:01:38 +0300

    Initial Commit

---
+++ StackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StackView
+//
+//  Created by Belma İbrahimbaş on 5.08.2017.
+//  Copyright © 2017 Belma Ibrahimbas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
